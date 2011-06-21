4a48e8aff04139ef403157093cbe7d8a73d4159b
Jeffrey Hill <jeffrey.r.hill@gmail.com>
2011-06-21 18:42:42 -0400

    Blank out as to yet unestablished services

--- svc/publish.php
+++ svc/publish.php
@@ -6,26 +6,5 @@ defined('LREXEC') or die('Access Denied');
 
 class LRPublish extends LRService
 {
-    protected function add()
-    {
-        parent::setActivity('add');
-        parent::execute();
-        $response = parent::getResponse();
-        return (int) $response->DATA;
-    }
-    
-    protected function obtain()
-    {
-        parent::setActivity('obtain');
-        parent::execute();
-        return parent::getResponse();
-    }
-    
-    protected function publish()
-    {
-        parent::setActivity('publish');
-        parent::execute();
-        return parent::getResponse('array');
-    }
     
 }
