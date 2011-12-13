2632bc423a2d91a41e83c7225ef96ddf312777c5
Jason Hoekstra <jasonhoekstra@gmail.com>
2011-12-13 17:16:37 -0500

    Adding additional shell unit test files.

---
+++ tests/lr.php
@@ -0,0 +1,14 @@
+<?php
+// @package LR-PHP
+// @copyright 2011 Jeffrey Hill
+// @license Apache 2.0 License http://www.apache.org/licenses/LICENSE-2.0.html
+require_once('../lr.php');
+
+class LRTest extends PHPUnit_Framework_TestCase 
+{
+  public function lr() {
+    //TODO:  Convert this to an actual PHPUnitTest
+  }
+}
+
+?>
