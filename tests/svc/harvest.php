50abeb5d8dcaf070279388914d5afc32211fde2a
Jason Hoekstra <jasonhoekstra@gmail.com>
2011-12-13 17:15:29 -0500

    Adding additional shell unit test files.

--- tests/svc/harvest.php
+++ tests/svc/harvest.php
@@ -2,24 +2,34 @@
 // @package LR-PHP
 // @copyright 2011 Jeffrey Hill
 // @license Apache 2.0 License http://www.apache.org/licenses/LICENSE-2.0.html
-require_once('../lr.php');
-LR::init('harvest');
-LR::setVerb('getrecord');
-LR::setArgs(array(
-    'request_ID'=>'b3398a58f0ff417bb3e56e20e7edc787'
-));
-$e = LR::execute("POST");
-if($e == true)
-{
-    print_r(LR::getResponse());
-} else print_r(LR::getErrors());
+require_once('../../lr.php');
 
-LR::init('harvest');
-LR::setVerb('listrecords');
-LR::setArgs(array(
-    'from'=>'2009-01-01',
-    'until'=>'2011-06-01'
-));
-$e = LR::execute("POST");
-print_r(LR::getResponse());
+class LRHarvestTest extends PHPUnit_Framework_TestCase 
+{
+  public function harvest() {
+  //TODO:  Convert this to an actual PHPUnitTest
+    
+    LR::init('harvest');
+    LR::setVerb('getrecord');
+    LR::setArgs(array(
+        'request_ID'=>'b3398a58f0ff417bb3e56e20e7edc787'
+    ));
+    $e = LR::execute("POST");
+    if($e == true) {
+      print_r(LR::getResponse());
+    } 
+    else {
+      print_r(LR::getErrors());
+    }
+
+    LR::init('harvest');
+    LR::setVerb('listrecords');
+    LR::setArgs(array(
+        'from'=>'2009-01-01',
+        'until'=>'2011-06-01'
+    ));
+    $e = LR::execute("POST");
+    print_r(LR::getResponse());
+  }
+}
 ?>
--- tests/svc/obtain.php
+++ tests/svc/obtain.php
@@ -1,13 +1,24 @@
 <?php
-require_once('../lr.php');
-LR::init('obtain');
-LR::setArgs(array(
-    'by_resource_ID'=>true,
-    'request_IDs'=>array('b3398a58f0ff417bb3e56e20e7edc787')
-));
-$e = LR::execute();
-if($e == true)
+// @package LR-PHP
+// @copyright 2011 Jeffrey Hill
+// @license Apache 2.0 License http://www.apache.org/licenses/LICENSE-2.0.html
+require_once('../../lr.php');
+
+class LRObtainTest extends PHPUnit_Framework_TestCase
 {
-    print_r(LR::getResponse());
-} else print_r(LR::getErrors());
+    public function getRecord()
+    {
+      //TODO:  Convert this to an actual PHPUnitTest
+      LR::init('obtain');
+      LR::setArgs(array(
+          'by_resource_ID'=>true,
+          'request_IDs'=>array('b3398a58f0ff417bb3e56e20e7edc787')
+      ));
+      $e = LR::execute();
+      if($e == true)
+      {
+          print_r(LR::getResponse());
+      } else print_r(LR::getErrors());
+    }
+}
 ?>
