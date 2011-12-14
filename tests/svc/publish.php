d779a0629a93e85910e7e44315eedca3923b9131
Jeffrey Hill <jeffreyr.hill@gmail.com>
2011-12-14 11:41:48 -0500

    LRDIR in LRPublish test

--- tests/svc/publish.php
+++ tests/svc/publish.php
@@ -2,8 +2,8 @@
 // @package LR-PHP
 // @copyright 2011 Jeffrey Hill
 // @license Apache 2.0 License http://www.apache.org/licenses/LICENSE-2.0.html
-require_once('../../lr.php');
-require_once('../../config.php');
+require_once(LRDIR.DS.'lr.php');
+require_once(LRDIR.DS.'config.php');
 
 class LRPublishTest
 {
