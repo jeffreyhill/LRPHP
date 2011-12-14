f8e29166cb979615a47f4e9ee4002b7076c53ea5
Jeffrey Hill <jeffreyr.hill@gmail.com>
2011-12-14 13:03:43 -0500

    LRDocument and LRSignature OOing

--- lib/signature.php
+++ lib/signature.php
@@ -1,19 +1,23 @@
 <?php
 require_once 'Crypt/GPG.php';
-require_once 'bencode.php';
+require_once LRDIR.DS.'lib'.DS.'bencode.php';
 
-class LRSignature {
-
-	static function sign_file($filedata){
+class LRSignature
+{
+	static function sign_file($filedata)
+	{
 		$gpg = new Crypt_GPG();
 		$gpg->addSignKey('wegrata@gmail.com');
 		$signature = $gpg->sign($filedata, Crypt_GPG::SIGN_MODE_CLEAR);
 		return $signature;
 	}
-	static function import_key($key_url){
+	
+	static function import_key($key_url)
+	{
 		$gpg = new Crypt_GPG();
 		$gpg->importKeyFile($key_url);
 	}
+	
 	static function normalize_data($data)
 	{
 		if (is_null($data)){
@@ -29,74 +33,85 @@ class LRSignature {
 		}		
 		return $data;
 	}
-	static function format_data_to_sign($rawData){
+	
+	static function format_data_to_sign(LRDocument $document){
 	    
-		unset($rawData['digital_signature']);
+		unset($document['digital_signature']);
 	
-		unset($rawData['_id']);
-		unset($rawData['_rev']);
+		unset($document['_id']);
+		unset($document['_rev']);
 	
-		unset($rawData['doc_ID']);
-		unset($rawData['publishing_node']);
-		unset($rawData['update_timestamp']);
-		unset($rawData['node_timestamp']);
-		unset($rawData['create_timestamp']);
+		unset($document['doc_ID']);
+		unset($document['publishing_node']);
+		unset($document['update_timestamp']);
+		unset($document['node_timestamp']);
+		unset($document['create_timestamp']);
 	
-		$rawData = normalize_data($rawData);
+		$document = self::normalize_data($document);
 		$encoder = new bencoding();
-		$data = utf8_encode($encoder->encode($rawData));
+		$data = utf8_encode($encoder->encode($document));
 		$hash = hash('SHA256',$data);
-		return $hash;	
+		return $hash;
 	}
-	static function verify_file($filedata, $signature){
+	
+	static function verify_file($signature)
+	{
 		$gpg = new Crypt_GPG();
-		$results = $gpg->verify( $signature);	
+		$results = $gpg->verify($signature);
 		return $results;
 	}
-	static function read_file($filename){
+	
+	static function read_file($filename)
+	{
 		$handle = fopen($filename,'r');
 		$data = fread($handle,filesize($filename));
 		fclose($handle);
 		return $data;
 	}
-	function write_file($filename,$data){
+	
+	function write_file($filename,$data)
+	{
 		$handle = fopen($filename,'w');
 		fwrite($handle,$data);
 		fclose($handle);
 	}
-	function get_hash_from_signature($signature){
+	
+	function get_hash_from_signature($signature)
+	{
 		$parts = preg_split('[\r|\n]',$signature);	
 		return $parts[3];
 	}
-	function test_verify($filename){
-		$data = read_file($filename);
-		$json = json_decode($data, true);
-		import_key($json['digital_signature']['key_location'][0]);
-		$signature = $json['digital_signature']['signature'];
-		$testHash = get_hash_from_signature($signature);
-		$signData = format_data_to_sign($json);
-		if($signData == $testHash){
-			$resultList = verify_file($data, $signature);
+	
+	function test_verify(LRDocument $document)
+	{
+		self::import_key($document['digital_signature']['key_location'][0]);
+		$signature = $document['digital_signature']['signature'];
+		$testHash = self::get_hash_from_signature($signature);
+		$signData = self::format_data_to_sign($document);
+		if($signData == $testHash) {
+			$resultList = self::verify_file($signature);
 			$result = $resultList[0];
-			if( $result->isValid()){
-				echo "Valid", "\n";
+			if($result->isValid()){
+				return true;
 			}
 		}
+		return false;
 	}
-	function test_signing($filename,$outfile){
-		$data = read_file($filename);
-		$json = json_decode($data, true);
-		$signData = format_data_to_sign($json);
-		$signature = sign_file($signData);
-		$json['digital_signature'] = array(
+	
+	function test_signing(LRDocument $document)
+	{
+		$hash = self::format_data_to_sign($document);
+		$signature = self::sign_file($hash);
+		$document['digital_signature'] = array(
 			'key_location' => array('http://12.109.40.15/resource_data/key/key.asc'),
 			'signature' => $signature,
 			'key_owner' => 'wegrata@gmail.com',
 			'signing_method' => 'LR-PGP.1.0'
 		);
-		$jsonString = json_encode($json);
-		write_file($outfile,$jsonString);
+		return $document;
 	}
+
+}
 
 	$testFile = '2011-02-28Metadata10.json';
 	$signedFile = 'signedOutput.json';
