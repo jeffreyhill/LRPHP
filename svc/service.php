cc8a07afcefe3328d7cbc367e85d00083f6c0a88
Jason Hoekstra <jasonhoekstra@gmail.com>
2011-12-14 01:47:06 -0500

    Update on service.php to specify exact include path.  Implemented testVerb on service.php test case.

--- svc/service.php
+++ svc/service.php
@@ -3,8 +3,8 @@
 // @copyright 2011 Jeffrey Hill
 // @license Apache 2.0 License http://www.apache.org/licenses/LICENSE-2.0.html
 defined('LREXEC') or die('Access denied');
-require_once('..'.DS.'request.php');
-require_once('..'.DS.'utility.php');
+require_once(dirname(__FILE__).DS. '..'.DS.'request.php');
+require_once(dirname(__FILE__).DS.'..'.DS.'utility.php');
 
 class LRService
 {
--- tests/svc/service.php
+++ tests/svc/service.php
@@ -2,13 +2,18 @@
 // @package LR-PHP
 // @copyright 2011 Jeffrey Hill
 // @license Apache 2.0 License http://www.apache.org/licenses/LICENSE-2.0.html
-require_once('../../lr.php');
+require_once(dirname(__FILE__) . '/../../lr.php');
+require_once(dirname(__FILE__) . '/../../svc/service.php');
 
 class LRServiceTest extends PHPUnit_Framework_TestCase 
 {
-  public function testService() {
-    //TODO:  Convert this to an actual PHPUnitTest
-    $this->assertFalse(TRUE);
+  
+  public function testVerb() 
+  {
+    $lrService = new LRService;
+    $lrService->setVerb("default");
+    $result = $lrService->getVerb();
+    $this->assertEquals("default", $result);
   }
 }
 
